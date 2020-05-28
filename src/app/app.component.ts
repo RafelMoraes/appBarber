1c6fa0c259252477e1e802b3fa4f20d61fb8d424
RafaelMoraes08 <rafamoraes5550@gmail.com>
2020-05-28 00:41:37 -0300

    Criação layouts

---
+++ src/app/app.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent implements OnInit {
+  public selectedIndex = 0;
+  public appPages = [
+    {
+      title: 'Inicio',
+      url: '/inicio',
+      icon: 'home'
+    },
+    {
+      title: 'Cadastro',
+      url: '/cadastro',
+      icon: 'create'
+    },
+    {
+      title: 'Agendamento',
+      url: '/agendamento',
+      icon: 'calendar'
+    },
+    {
+      title: 'Perfil',
+      url: '/perfil',
+      icon: 'person'
+    },
+    {
+      title: 'Contato',
+      url: '/contato',
+      icon: 'call'
+    },
+    {
+      title: 'Sair',
+      url: '/folder/folder',
+      icon: 'exit'
+    }
+  ];
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  ngOnInit() {
+    const path = window.location.pathname.split('folder/')[1];
+    if (path !== undefined) {
+      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+    }
+  }
+}
